384ccd28e9d009da5e1d9c12b5d871d611321996
LaurenLind1 <snowbunny1126@icloud.com>
2026-06-11 09:06:37 -0700

    Update p.$projectId.tsx

--- src/routes/p.$projectId.tsx
+++ src/routes/p.$projectId.tsx
@@ -191,4 +191,4 @@ function Dashboard() {
 
         {/* Right Code Canvas */}
         <div className="flex flex-1 flex-col bg-background">
-          <div className="flex items-center justify-between border-b px-4 py-2 bg-
+          <div className="flex items-center justify-between border-b px-4 py-2">

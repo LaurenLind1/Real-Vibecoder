99025fed5a6297859ed561f3cbf92249fedfccdc
LaurenLind1 <snowbunny1126@icloud.com>
2026-06-11 08:06:30 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from "@tanstack/start/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });

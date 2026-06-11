a424d561b3cef86fb20bb94b46109d4ae70de29f
LaurenLind1 <snowbunny1126@icloud.com>
2026-06-10 18:42:59 -0700

    Update routeTree.gen.ts

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -1,1 +1,29 @@
+/* eslint-disable */
 
+// @ts-nocheck
+
+// noinspection JSUnusedGlobalSymbols
+
+// This file was safely manually generated to fix the routing tree sync issue.
+
+import { Route as rootRoute } from './routes/__root'
+import { Route as IndexImport } from './routes/index'
+
+const IndexRoute = IndexImport.update({
+  path: '/',
+  getParentRoute: () => rootRoute,
+} as any)
+
+declare module '@tanstack/react-router' {
+  interface FileRoutesByPath {
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexImport
+      parentRoute: typeof rootRoute
+    }
+  }
+}
+
+export const routeTree = rootRoute._addFileChildren({ IndexRoute })
